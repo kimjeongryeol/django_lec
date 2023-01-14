96d3c319f77c43441ce038e91b390a1348ec9f1d
kim <wjdfuf0621@naver.com>
2023-01-15 02:13:50 +0900

    4주차 - 1

--- mysite/models.py
+++ mysite/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class MainContent(models.Model):
     title =  models.CharField(max_length=500)
     content = models.TextField()
     pub_date = models.DateTimeField('data published')
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_list = models.ForeignKey(MainContent,on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    modify_date = models.DateTimeField(auto_now=True)

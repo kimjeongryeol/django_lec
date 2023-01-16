058e8ae71564a42cf15a69447797bd3bef5a81f3
kim <wjdfuf0621@naver.com>
2023-01-16 19:17:34 +0900

    4주차 -2

--- mysite/views.py
+++ mysite/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import MainContent, Comment
 from .forms import CommentForm
+
+
 # Create your views here.
 
 @login_required(login_url='accounts:login')
@@ -12,17 +14,18 @@ def comment_update(request, comment_id):
     if request.user != comment.author:
         raise PermissionDenied
 
-    if request.method =='POST':
+    if request.method == 'POST':
         form = CommentForm(request.POST, instance=comment)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.save()
             return redirect('detail', content_id=comment.content_list.id)
-        else:
-            form = CommentForm(instance=comment)
+    else:
+        form = CommentForm(instance=comment)
 
-        context = {'comment': comment, 'form':form}
-        return render(request, 'mysite/comment_form.html', context)
+    context = {'comment': comment, 'form': form}
+    return render(request, 'mysite/comment_form.html', context)
+
 
 @login_required(login_url='account:login')
 def comment_delete(request, comment_id):
@@ -34,22 +37,24 @@ def comment_delete(request, comment_id):
         comment.delete()
     return redirect('detail', content_id=comment.content_list.id)
 
+
 def index(request):
     content_list = MainContent.objects.order_by('-pub_date')
     context = {'content_list': content_list}
-    return render(request,   'mysite/content_list.html', context)
+    return render(request, 'mysite/content_list.html', context)
+
 
 def detail(request, content_id):
-
     content_list = get_object_or_404(MainContent, pk=content_id)
-    context = {'content_list':content_list}
+    context = {'content_list': content_list}
     return render(request, 'mysite/content_detail.html', context)
+
 
 @login_required(login_url='accounts:login')
 def comment_create(request, content_id):
     content_list = get_object_or_404(MainContent, pk=content_id)
 
-    if request.method =='POST':
+    if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
@@ -57,7 +62,8 @@ def comment_create(request, content_id):
             comment.author = request.user
             comment.save()
             return redirect('detail', content_id=content_list.id)
-        else:
-            form = CommentForm()
-            context = {'content_list': content_list, 'form': form}
-            return render(request, 'mysite/content_detail.html', context)
+    else:
+        form = CommentForm()
+
+    context = {'content_list': content_list, 'form': form}
+    return render(request, 'mysite/content_detail.html', context)
